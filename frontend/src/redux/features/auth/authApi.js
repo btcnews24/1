112a90d82a3b794674816545da562219d03e0de0
btcnews24 <ameenahamd308@gmail.com>
2024-12-23 08:53:56 +0600

    Update authApi.js

--- frontend/src/redux/features/auth/authApi.js
+++ frontend/src/redux/features/auth/authApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 const authApi = createApi({
     reducerPath: 'authApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:3000/api/user',
+        baseUrl: 'https://1-production-3924.up.railway.app/api/user',
         credentials: 'include',
     }),
     endpoints: (builder) => ({
